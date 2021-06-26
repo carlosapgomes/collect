069993c5dc8bad2f3cd35201ef2f3285dd3ac2b0
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-06-25 21:12:17 -0300

    feat: wire feathers client and superagent to make rest requests

--- collect-client/src/CollectClient.js
+++ collect-client/src/CollectClient.js
@@ -1,61 +1,34 @@
-import { LitElement, html, css } from 'lit-element';
-import { openWcLogo } from './open-wc-logo.js';
+import { LitElement, html } from 'lit-element';
 
 export class CollectClient extends LitElement {
+  
   static get properties() {
     return {
       title: { type: String },
     };
   }
 
-  static get styles() {
-    return css`
-      :host {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-        background-color: var(--collect-client-background-color);
-      }
-
-      main {
-        flex-grow: 1;
-      }
-
-      .logo > svg {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
-      }
-    `;
-  }
-
   constructor() {
     super();
     this.title = 'My app';
+    this.app = {};
+    this.restClient = {};
+  }
+
+  // use lightDOM
+  createRenderRoot(){
+    return this;
+  }
+
+  firstUpdated(){
+    // grab the global feathers object imported on index.html
+    this.app = window.feathers();
+    this.restClient = window.feathers.rest('http://localhost:3030');
+    this.app.configure(this.restClient.superagent(window.superagent));
+    const proceduresSvc = this.app.service('procedures');
+    proceduresSvc.find().then((res)=>{
+      console.log(res);
+    });
   }
 
   render() {
