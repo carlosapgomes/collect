24e0736abd4f7c64f8ebcdd36af8f9233397c1b7
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-07-12 07:30:27 -0300

    style: fix indentation

--- collect-client/src/icons/IconEdit.js
+++ collect-client/src/icons/IconEdit.js
@@ -8,24 +8,24 @@ export class IconEdit extends LitElement {
 
   render() {
     return html`
-                              <span class="icon is-small is-right">
-                                <svg
-                                  id="i-edit"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 32 32"
-                                  width="16"
-                                  height="16"
-                                  fill="none"
-                                  stroke="currentcolor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                >
-                                  <path
-                                    d="M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z"
-                                  />
-                                </svg>
-                              </span>
+      <span class="icon is-small is-right">
+        <svg
+          id="i-edit"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          width="16"
+          height="16"
+          fill="none"
+          stroke="currentcolor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z"
+          />
+        </svg>
+      </span>
     `;
   }
 }
--- collect-client/src/icons/IconTrash.js
+++ collect-client/src/icons/IconTrash.js
@@ -8,24 +8,24 @@ export class IconTrash extends LitElement {
 
   render() {
     return html`
-                              <span class="icon is small is-right">
-                                <svg
-                                  id="i-trash"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 32 32"
-                                  width="16"
-                                  height="16"
-                                  fill="none"
-                                  stroke="currentcolor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                >
-                                  <path
-                                    d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6"
-                                  />
-                                </svg>
-                              </span>
+      <span class="icon is small is-right">
+        <svg
+          id="i-trash"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          width="16"
+          height="16"
+          fill="none"
+          stroke="currentcolor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6"
+          />
+        </svg>
+      </span>
 `;
   }
 }
