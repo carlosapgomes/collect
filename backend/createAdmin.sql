9d4b5619730a3c34243d750467cf0cde50997d5b
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-07-18 12:37:59 -0300

    feat: update sql script to create first user and isolate doctors related code in the backend

--- backend/createAdmin.sql
+++ backend/createAdmin.sql
@@ -1,2 +1,2 @@
 -- create an admin user with password '1234'
-insert into users(id,name,email,phone,username,password,isAdmin,isDoctor,isEnabled,changePassword,createdAt,updatedAt) values(1,'admin','admin@user.domain','1234567','admin','$2a$10$N7OxArYhN7X0QCoEk/O7jO8SdOUuwKJBNSCNY3mFq7yUOP.k/VJ02',1,1,1,1,datetime(),datetime());
+insert into users(id,name,email,phone,username,password,isAdmin,isEnabled,changePassword,profBoardName,licenceNumber,createdAt,updatedAt) values(1,'admin','admin@user.domain','1234567','admin','$2a$10$N7OxArYhN7X0QCoEk/O7jO8SdOUuwKJBNSCNY3mFq7yUOP.k/VJ02',1,1,1,'CRM','1234',datetime(),datetime());
--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,13 +1,13 @@
 const users = require('./users/users.service.js');
 const procedures = require('./procedures/procedures.service.js');
 const proctypes = require('./proctypes/proctypes.service.js');
-const doctors = require('./doctors/doctors.service.js');
+//const doctors = require('./doctors/doctors.service.js');
 const patients = require('./patients/patients.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(procedures);
   app.configure(proctypes);
-  app.configure(doctors);
+  //app.configure(doctors);
   app.configure(patients);
 };
