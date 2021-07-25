b32005d3dd1734b530e9b1fe636b7b8d8fa629cd
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-07-25 10:10:51 -0300

    feat: add rollup copy plugin to move assets to dist folder

--- collect-client/rollup.config.js
+++ collect-client/rollup.config.js
@@ -1,6 +1,7 @@
 import merge from 'deepmerge';
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
+import copy from 'rollup-plugin-copy';
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -24,7 +25,14 @@ export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
-
+  plugins: [
+    copy({
+      targets: [
+        { src: 'css/*', dest: 'dist/css' },
+        { src: 'assets/*', dest: 'dist/assets' },
+      ],
+    }),
+  ],
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
   // input: './app.js',
