d9ee779b79244817b32225be362297deabd09ce2
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-07-28 11:06:35 -0300

    fix: check for the same data type

--- backend/src/hooks/is-same-user-or-admin.js
+++ backend/src/hooks/is-same-user-or-admin.js
@@ -4,10 +4,10 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
-    if ((context.params.user) &&
+    if ((context.params.user && context.id) &&
       (
         (context.params.user.isAdmin === 1) ||
-        ((context.id) && (context.id === context.user.id)))
+        ((parseInt(context.id,10) === context.params.user.id)))
     ) {
       return context;
     }
