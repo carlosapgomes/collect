217ea57781ab79fb2c42859ab0e1c0c40eef3f5b
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-07-28 21:18:13 -0300

    feat: add icons to page navigation component

--- collect-client/src/PageNav.js
+++ collect-client/src/PageNav.js
@@ -9,17 +9,17 @@ export class PageNav extends LitElement {
 
   static get properties() {
     return {
-      total: {type: Number},
-      limit: {type: Number},
-      skip: {type: Number},
-      _currentPage: {type: Number, state: true},
-      _lastPage: {type: Number, state: true},
-      _nextPage: {type: Number, state: true},
-      _prevPage: {type: Number, state: true},
-    }
+      total: { type: Number },
+      limit: { type: Number },
+      skip: { type: Number },
+      _currentPage: { type: Number, state: true },
+      _lastPage: { type: Number, state: true },
+      _nextPage: { type: Number, state: true },
+      _prevPage: { type: Number, state: true },
+    };
   }
 
-  constructor(){
+  constructor() {
     super();
     this._updatePages();
   }
@@ -30,19 +30,20 @@ export class PageNav extends LitElement {
     }
   }
 
-  _updatePages(){
-    this._currentPage = parseInt((this.skip/this.limit),10) + 1;
-    this._lastPage = parseInt((this.total/this.limit),10);
-    if((this.total%this.limit) !== 0){
+  _updatePages() {
+    this._currentPage = parseInt(this.skip / this.limit, 10) + 1;
+    this._lastPage = parseInt(this.total / this.limit, 10);
+    if (this.total % this.limit !== 0) {
       this._lastPage += 1;
     }
-    this._nextPage = (this._currentPage === this._lastPage) ?
-      this._lastPage : (this._currentPage +1);
-    this._prevPage = (this._currentPage === 1) ?
-      1 : (this._currentPage -1);
+    this._nextPage =
+      this._currentPage === this._lastPage
+        ? this._lastPage
+        : this._currentPage + 1;
+    this._prevPage = this._currentPage === 1 ? 1 : this._currentPage - 1;
   }
 
-  _gotoFirstPage(){
+  _gotoFirstPage() {
     this.dispatchEvent(
       new CustomEvent('paginate', {
         detail: {
@@ -54,21 +55,21 @@ export class PageNav extends LitElement {
     );
   }
 
-  _gotoLastPage(){
+  _gotoLastPage() {
     this.dispatchEvent(
       new CustomEvent('paginate', {
         detail: {
-          skip: parseInt((this.total/this.limit),10)*this.limit,
+          skip: parseInt(this.total / this.limit, 10) * this.limit,
         },
         bubbles: true,
         composed: true,
       })
-    );  
+    );
   }
 
-  _gotoPrevPage(){
+  _gotoPrevPage() {
     let skip = this.skip - this.limit;
-    if (skip < 0 ){
+    if (skip < 0) {
       skip = 0;
     }
     this.dispatchEvent(
@@ -82,9 +83,9 @@ export class PageNav extends LitElement {
     );
   }
 
-  _gotoNextPage(){
+  _gotoNextPage() {
     let skip = this.skip + this.limit;
-    if (skip > this.total){
+    if (skip > this.total) {
       // we are already at the last page
       skip = this.skip;
     }
@@ -99,67 +100,136 @@ export class PageNav extends LitElement {
     );
   }
 
-
   render() {
-    return html`<nav class="pagination is-small" role="navigation" aria-label="pagination">
-      <button 
+    return html`<nav
+      class="pagination is-small"
+      role="navigation"
+      aria-label="pagination"
+    >
+      <button
         class="pagination-previous button"
         ?disabled="${this.skip === 0}"
-        @click="${this._gotoPrevPage}">Previous</button>
-      <button class="pagination-next button"
+        @click="${this._gotoPrevPage}"
+      >
+        <span class="icon">
+          <svg
+            id="i-caret-left"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            width="32"
+            height="32"
+            fill="none"
+            stroke="currentcolor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M22 30 L6 16 22 2 Z" />
+          </svg>
+        </span>
+      </button>
+      <button
+        class="pagination-next button"
         ?disabled="${this._currentPage === this._lastPage}"
-        @click="${this._gotoNextPage}">Next page</button>
+        @click="${this._gotoNextPage}"
+      >
+        <span class="icon">
+          <svg
+            id="i-caret-right"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            width="32"
+            height="32"
+            fill="none"
+            stroke="currentcolor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M10 30 L26 16 10 2 Z" />
+          </svg>
+        </span>
+      </button>
       <ul class="pagination-list">
-        <li><button 
-          class="pagination-link button ${classMap({
-          'is-hidden':  (this._currentPage === 1),
-          })}" 
-          aria-label="Goto page 1"
-          @click="${this._gotoFirstPage}"
-        >1</button></li>
-        <li><span class="pagination-ellipsis ${classMap({
-          'is-hidden': ( (this._currentPage === 1)||
-          ((this._currentPage - 1) === 1) || 
-          (this. _prevPage === 2)),
-          })}">&hellip;</span></li>
-        <li><button 
-          class="pagination-link button ${classMap({
-          'is-hidden': ( (this._currentPage === 1) ||
-          ((this._currentPage - 1) === 1)),
-          })}" 
-          aria-label="Goto previous page"
-          @click="${this._gotoPrevPage}"
-        >${this._prevPage}</button></li>
-        <li><button 
-          class="pagination-link is-current button" 
-          aria-label="current page" 
-          aria-current="page"
-        >${this._currentPage}</button></li>
-        <li><button 
-          class="pagination-link button ${classMap({
-          'is-hidden': (this._currentPage === this._lastPage),
-          })}" 
-          aria-label="Goto page 47"
-          @click="${this._gotoNextPage}"
-        >${this._nextPage}</button></li>
-        <li><span class="pagination-ellipsis ${classMap({
-          'is-hidden': ( (this._currentPage === this._lastPage)||
-          this._currentPage === (this._lastPage -1) || (
-          this._nextPage === (this._lastPage - 1)
-          )),
-          })}">&hellip;</span></li>
-        <li><button 
-          class="pagination-link button ${classMap({
-          'is-hidden': ( (this._currentPage === this._lastPage)||
-          (this._currentPage === (this._lastPage - 1))),
-          })}"
-          aria-label="Goto page 86"
-          @click="${this._gotoLastPage}"
-        >${this._lastPage}</button></li>
+        <li>
+          <button
+            class="pagination-link button ${classMap({
+              'is-hidden': this._currentPage === 1,
+            })}"
+            aria-label="Goto page 1"
+            @click="${this._gotoFirstPage}"
+          >
+            1
+          </button>
+        </li>
+        <li>
+          <span
+            class="pagination-ellipsis ${classMap({
+              'is-hidden':
+                this._currentPage === 1 ||
+                this._currentPage - 1 === 1 ||
+                this._prevPage === 2,
+            })}"
+            >&hellip;</span
+          >
+        </li>
+        <li>
+          <button
+            class="pagination-link button ${classMap({
+              'is-hidden':
+                this._currentPage === 1 || this._currentPage - 1 === 1,
+            })}"
+            aria-label="Goto previous page"
+            @click="${this._gotoPrevPage}"
+          >
+            ${this._prevPage}
+          </button>
+        </li>
+        <li>
+          <button
+            class="pagination-link is-current button"
+            aria-label="current page"
+            aria-current="page"
+          >
+            ${this._currentPage}
+          </button>
+        </li>
+        <li>
+          <button
+            class="pagination-link button ${classMap({
+              'is-hidden': this._currentPage === this._lastPage,
+            })}"
+            aria-label="Goto page 47"
+            @click="${this._gotoNextPage}"
+          >
+            ${this._nextPage}
+          </button>
+        </li>
+        <li>
+          <span
+            class="pagination-ellipsis ${classMap({
+              'is-hidden':
+                this._currentPage === this._lastPage ||
+                this._currentPage === this._lastPage - 1 ||
+                this._nextPage === this._lastPage - 1,
+            })}"
+            >&hellip;</span
+          >
+        </li>
+        <li>
+          <button
+            class="pagination-link button ${classMap({
+              'is-hidden':
+                this._currentPage === this._lastPage ||
+                this._currentPage === this._lastPage - 1,
+            })}"
+            aria-label="Goto page 86"
+            @click="${this._gotoLastPage}"
+          >
+            ${this._lastPage}
+          </button>
+        </li>
       </ul>
     </nav>`;
   }
 }
-
-
-
