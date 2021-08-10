c326534c428286a29d09b48890ddfe75c40409ba
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-08-10 20:37:51 -0300

    feat: add users view dynamic search and pagination

--- collect-client/src/UsersView.js
+++ collect-client/src/UsersView.js
@@ -3,6 +3,7 @@ import { classMap } from 'lit-html/directives/class-map.js';
 import './btn-fab.js';
 import './page-nav.js';
 import './icons/icon-edit.js';
+import './icons/icon-reload.js';
 
 export class UsersView extends LitElement {
   // use lightDOM
@@ -12,11 +13,12 @@ export class UsersView extends LitElement {
 
   static get properties() {
     return {
-      usersres: {type: Object},
+      usersres: { type: Object },
       _users: { type: Array, attribute: true },
-      _total: {type: Number, state: true},
-      _limit: {type: Number, state: true},
-      _skip: {type: Number, state: true},
+      _total: { type: Number, state: true },
+      _limit: { type: Number, state: true },
+      _skip: { type: Number, state: true },
+      _searchFor: { type: String, state: true },
     };
   }
 
@@ -24,18 +26,20 @@ export class UsersView extends LitElement {
     super();
     /** @type {object[]} */
     this._users = [];
+    this._searchFor = '';
   }
 
   firstUpdated() {
     this.dispatchEvent(
-      new CustomEvent('update-users-list', { 
-        detail:{
+      new CustomEvent('update-users-list', {
+        detail: {
           skip: 0,
         },
-        bubbles: true, 
-        composed: true })
+        bubbles: true,
+        composed: true,
+      })
     );
-    this.addEventListener('paginate',this._paginate);
+    this.addEventListener('paginate', this._paginate);
   }
 
   updated(changedProperties) {
@@ -49,17 +53,22 @@ export class UsersView extends LitElement {
     }
   }
 
-  _paginate(e){
+  _paginate(e) {
     e.stopPropagation();
+    const detail = { skip: e.detail.skip };
+    let eventName = 'update-users-list';
+    if (this._searchFor.length > 2) {
+      detail.search = this._searchFor;
+      eventName = 'search-user';
+    }
     this.dispatchEvent(
-      new CustomEvent('update-users-list', {
-        detail:{
-          skip: e.detail.skip,
-        },
+      new CustomEvent(eventName, {
+        detail,
         bubbles: true,
         composed: true,
       })
-    );  }
+    );
+  }
 
   _edit(u) {
     // eslint-disable-next-line no-console
@@ -75,30 +84,123 @@ export class UsersView extends LitElement {
     );
   }
 
+  _searchUser(e) {
+    this._searchFor = e.target.value;
+    // eslint-disable-next-line no-console
+    console.log(`searching for: ${e.target.value}`);
+    // fire event to hide procedure form from parent's view
+    if (e.target.value.length === 0) {
+      this.dispatchEvent(
+        new CustomEvent('update-users-list', {
+          detail: {
+            skip: 0,
+          },
+          bubbles: true,
+          composed: true,
+        })
+      );
+    }
+
+    if (e.target.value.length > 2) {
+      this.dispatchEvent(
+        new CustomEvent('search-user', {
+          detail: {
+            search: e.target.value,
+            skip: 0,
+          },
+          bubbles: true,
+          composed: true,
+        })
+      );
+    }
+  }
+
+  _refreshSearch() {
+    if (this._searchFor.length === 0) {
+      // just update patients list
+      this.dispatchEvent(
+        new CustomEvent('update-users-list', {
+          detail: {
+            skip: 0,
+          },
+          bubbles: true,
+          composed: true,
+        })
+      );
+    } else if (this._searchFor.length > 2) {
+      this.dispatchEvent(
+        new CustomEvent('search-user', {
+          detail: {
+            search: this._searchFor,
+            skip: 0,
+          },
+          bubbles: true,
+          composed: true,
+        })
+      );
+    }
+  }
+
   render() {
     return html`
       <style>
         .user-card {
           margin-bottom: 0.3em;
         }
-          svg {
-            margin: 0.4em;
-            overflow: visible;
+        svg {
+          margin: 0.4em;
+          overflow: visible;
         }
       </style>
-        <section id="users" class="section">
-          <div class="column is-6 is-offset-3">
-            <h1 class="subtitle has-text-centered is-3">Usuários</h1>
-            <div class="container
+      <section id="users" class="section">
+        <div class="column is-6 is-offset-3">
+          <h1 class="subtitle has-text-centered is-3">Usuários</h1>
+          <div
+            class="is-flex
+                is-flex-direction-row"
+          >
+            <div
+              class="control is-expanded has-icons-right
+                is-flex-grow-5"
+            >
+              <input
+                class="input"
+                type="search"
+                @keyup="${this._searchUser}"
+                @changed=${e => {
+                  this._searchFor = e.target.value;
+                }}
+                placeholder="buscar pelo nome ou registro profissional"
+                required
+              />
+              <icon-search></icon-search>
+            </div>
+            <div class="">
+              <button
+                class="button 
+                    is-ghost
+                    has-tooltip-arrow
+                    has-tooltip-top"
+                data-tooltip="Atualizar"
+                @click="${this._refreshSearch}"
+                @keydown="${this._refreshSearch}"
+              >
+                <icon-reload></icon-reload>
+              </button>
+            </div>
+          </div>
+          <div
+            class="container
               is-flex
               ${classMap({
-              'is-flex-direction-column-reverse': ( window.screen.width < 769 ),
-              'is-flex-direction-column': ( window.screen.width >= 769 ),
-              })}">
-              <div>
-                <br />
-                ${this._users
-                  ? this._users.map(
+              'is-flex-direction-column-reverse': window.screen.width < 769,
+              'is-flex-direction-column': window.screen.width >= 769,
+            })}"
+          >
+            <div>
+              <br />
+              ${this._users
+                ? this._users.map(
                     u => html`
                       <div class="card user-card">
                         <div class="card-content">
@@ -110,10 +212,10 @@ export class UsersView extends LitElement {
                               has-tooltip-top"
                               data-tooltip="Editar"
                               @click="${() => {
-                              this._edit(u);
+                                this._edit(u);
                               }}"
                               @keydown="${() => {
-                              this._edit(u);
+                                this._edit(u);
                               }}"
                             >
                               <icon-edit></icon-edit>
@@ -148,21 +250,22 @@ export class UsersView extends LitElement {
                       </div>
                     `
                   )
-                  : html`</p>`}
-              </div>
-              <page-nav
-                .total=${this._total}
-                .limit=${this._limit}
-                .skip=${this._skip}>
-              </page-nav>
+                : html`</p>`}
             </div>
+            <page-nav
+              .total=${this._total}
+              .limit=${this._limit}
+              .skip=${this._skip}
+            >
+            </page-nav>
           </div>
-          <btn-fab
-            @click="${() => {
+        </div>
+        <btn-fab
+          @click="${() => {
             this._addUser();
-            }}"
-          ></btn-fab>
-        </section>
+          }}"
+        ></btn-fab>
+      </section>
     `;
   }
 }
